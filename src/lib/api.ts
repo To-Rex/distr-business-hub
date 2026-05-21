4dc534a1581cd20e5bdf37e9c145a8f613f47a4d
To-Rex <torex.amaki@gmail.com>
2026-05-22 01:13:56 +0500

    recover work

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
-//const BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
-const BASE_URL = "https://distrbackend-backend-ik7zfv-29362a-83-149-105-190.sslip.io/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+//const BASE_URL = "https://distrbackend-backend-ik7zfv-29362a-83-149-105-190.sslip.io/api";
 //const BASE_URL = "https://distr.mxsoft.uz/api";
 
 if (!BASE_URL) {
